7ab534d90eb5ec01a9691622c052d910ef5ada04
ola <olanochristopher@gmail.com>
2020-02-24 01:29:06 +0800

    update db

--- controller/connect.php
+++ controller/connect.php
@@ -1,4 +1,4 @@
 <?php 
-    $conn =  mysqli_connect("localhost", "homestead", "secret", "edukasyon");
+    $conn =  mysqli_connect("g8r9w9tmspbwmsyo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "i64klbedp1alfugw", "to6p0h9u1qfzmr44", "cus9yndvup06eol3");
 
 ?>
--- model/Classes/ConnectDb.php
+++ model/Classes/ConnectDb.php
@@ -3,7 +3,7 @@
 		private $con;
 
 		public function dbConnection() {
-			$this->con = mysqli_connect("localhost", "homestead", "secret", "edukasyon");
+			$this->con = mysqli_connect("g8r9w9tmspbwmsyo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "i64klbedp1alfugw", "to6p0h9u1qfzmr44", "cus9yndvup06eol3");
 			return $this->con;
 		}
 	}
